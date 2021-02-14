17250d4a09b92cba72db69e99ff6f88ec1130620
Muhammad Tayab Noor <tayyabahmed57@yahoo.com>
2021-02-14 22:38:50 +0100

    Initial Files

---
+++ predict.py
@@ -0,0 +1,94 @@
+import cv2
+import numpy as np
+from time import sleep
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+classifier = load_model('./emotion_classifier_5ep.h5')
+face_classifier = cv2.CascadeClassifier('./Haarcascades/haarcascade_frontalface_default.xml')
+
+num_classes = 6
+batch_size = 16
+img_rows, img_cols = 48, 48
+
+#Loading Data
+train_data_dir = './fer2013/train'
+validation_data_dir = './fer2013/validation'
+
+# Let's use some data augmentaiton
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=45,
+      width_shift_range=0.3,
+      height_shift_range=0.3,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+# set our batch size (typically on most mid tier systems we'll use 16-32)
+
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        color_mode = 'grayscale',
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        color_mode = 'grayscale',
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+class_labels = validation_generator.class_indices
+class_labels = {v: k for k, v in class_labels.items()}
+classes = list(class_labels.values())
+print(class_labels)
+
+def face_detector(img):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return (0, 0, 0, 0), np.zeros((48, 48), np.uint8), img
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+
+    try:
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+    except:
+        return (x, w, y, h), np.zeros((48, 48), np.uint8), img
+    return (x, w, y, h), roi_gray, img
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    rect, face, image = face_detector(frame)
+    if np.sum([face]) != 0.0:
+        roi = face.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+
+        # make a prediction on the ROI, then lookup the class
+        preds = classifier.predict(roi)[0]
+        label = class_labels[preds.argmax()]
+        label_position = (rect[0] + int((rect[1] / 2)), rect[2] + 25)
+        cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+    else:
+        cv2.putText(image, "No Face Found", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+
+    cv2.imshow('All', image)
+    if cv2.waitKey(1) == 13:  # 13 is the Enter Key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
